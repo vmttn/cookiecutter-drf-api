0c9dce7f6073dbbe2229944540355b893ea67dfa
Valentin Matton <taq@protonmail.ch>
2020-11-28 13:56:03 +0100

    [django] Always get secret key from env

--- {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!&q5ee+m2gwzrdo(_$b2i$j2^j@=_qe2ws1dje_#des8i!1w8-"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
