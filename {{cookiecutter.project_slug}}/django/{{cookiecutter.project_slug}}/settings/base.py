1c3f84dd29fe037fd0f0756bd596b78e00ee2875
Valentin Matton <taq@protonmail.ch>
2020-11-30 17:30:02 +0100

    [django] Use postgres

--- {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "{{ cookiecutter.project_slug }}.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
