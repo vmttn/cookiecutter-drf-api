bf2373f5c233264ffc0782bac0260555a2a53bb2
Valentin Matton <taq@protonmail.ch>
2021-03-30 13:50:18 +0200

    [fix] Properly parse port in settings

--- {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/settings/base.py
@@ -88,7 +88,7 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+        "PORT": int(os.environ.get("POSTGRES_PORT", 5432)),
     }
 }
 
